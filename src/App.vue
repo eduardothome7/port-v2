590675ba10f4c273b3e3dfead25c1ba7cede6b32
eduardo <eduardocanninithome@gmail.com>
2019-09-30 11:45:27 -0300

    setup vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>Eduardo Thomé</h1>
+    <p>{{ msg }}</p>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <div v-for='section in sections'>
+        <h3>{{ section.nome }}</h3>
+        <div v-for='item in section.items'>
+          <h4>{{ item.nome }}</h4>
+          <p>{{ item.description }}</p>
+        </div>
+      </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function(){
+    return {
+      msg: 'Portfólio V2',
+      sections: [{
+        nome: 'Conhecimentos',
+        items: [
+          {nome:'PHP', description: 'Laravel, Cakephp, Lumen. Instalação, configuração e customização de Magento, Wordpress'},
+          {nome: 'Mobile', description: 'Android, Ionic'},
+          {nome: 'Front-end', description: 'Angular, Vue.js, jQuery, Gulp. Design de Layouts'}
+        ]
+      },
+      {
+        nome: 'Formação',
+        items: [
+          {
+            nome: 'Análise e Desenvolvimento de Sistemas - Senac', description: 'Jul/2012 - Jul/2016'
+          },
+          {
+            nome: 'Pós Graduação em Desenvolvimento de Dispositivos Móveis - Senac', description: 'Mar/2017 - Jun/2019',
+          }
+        ],
+      },
+      {
+        nome: 'Experiência',
+        items: [
+          {
+            nome: 'Estúdio Sis', description:'Desenvolvedor PHP - FullStack. Mar/2017 - presente'
+          },
+          {
+            nome: 'Rage Integrativa - estágio', description:'Desenvolvedor Front-end. Nov/2015 - Jul/2016'
+          },
+          {
+            nome: 'ADP Brasil - estágio', description:'Desenvolvedor Progress 4GL. Set/2014 - Set/2015'
+          },
+          { 
+            nome: 'Dell Computadores - estágio', description:'Suporte de Produção. Mai/2013 - Mai/2014'
+          }
+        ]
+      }
+      ]
+    }
   }
+  // components: {
+    // HelloWorld
+  // }
 }
 </script>
 
@@ -21,7 +74,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
